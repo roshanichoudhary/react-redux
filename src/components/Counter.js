eeae224790a935bf3ff910fc58f2119dec077160
Roshani <roshanishete07@gmail.com>
2023-09-22 16:20:49 +0530

    react redux

---
+++ src/components/Counter.js
@@ -0,0 +1,31 @@
+import { useDispatch, useSelector } from "react-redux";
+import classes from "./Counter.module.css";
+
+const Counter = () => {
+  const counter = useSelector((state) => state.counter);
+  const showCounter = useSelector((state) => state.showCounter);
+
+  const dispatch = useDispatch();
+
+  const increamentHandler = () => {
+    dispatch({ type: "increment" });
+  };
+  const decrementHandler = () => {
+    dispatch({ type: "decrement" });
+  };
+  const toggleCounterHandler = () => {
+    dispatch({ type: "toggle" });
+  };
+
+  return (
+    <main className={classes.counter}>
+      <h1>Redux Counter</h1>
+      {showCounter && <div className={classes.value}>{counter}</div>}
+      <button onClick={increamentHandler}>Increment</button>
+      <button onClick={decrementHandler}>Decrement</button>
+      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+    </main>
+  );
+};
+
+export default Counter;
---
+++ src/store/index.js
@@ -0,0 +1,27 @@
+import { createStore } from "redux";
+
+const counterReducer = (state = { counter: 0, showCounter: true }, action) => {
+  if (action.type === "increment") {
+    return {
+      counter: state.counter + 1,
+      showCounter: state.showCounter,
+    };
+  }
+  if (action.type === "decrement") {
+    return {
+      counter: state.counter - 1,
+      showCounter: state.showCounter,
+    };
+  }
+  if (action.type === "toggle") {
+    return {
+      counter: state.counter,
+      showCounter: !state.showCounter,
+    };
+  }
+  return state;
+};
+
+const store = createStore(counterReducer);
+
+export default store;
